1a83a3949b33237862efdf845ec10fe601298ecb
Tito <pedrotitojerial@gmail.com>
2025-04-22 19:35:19 -0400

    parte de comentarios de list.h

--- src/includes/list.h
+++ src/includes/list.h
@@ -3,18 +3,36 @@
 
 #include <stdbool.h>
 
-// Definicion nodos para este caso
+/**
+ * @brief Define la estructura de un nodo para la lista enlazada especifica de documentos.
+ * Cada nodo representa la aparicion de una palabra en el documento. 
+ */
 typedef struct node {
-    char* documento;
-    int frecuencia;
-    struct node * next;
+    char* documento;    // Identificador del documento.
+    int frecuencia;     // Frecuencia de aparicion de este documento.
+    struct node * next; // Puntero al siguiente nodo de la lista o NULL si es el ultimo.
 } node;
 
-// Definicion nodo puntero como tipo
+/**
+ * @brief Definicion de alias para la estructura "node" para su mejor lectura.
+ */
 typedef node* nodePtr;
 
-// Funciones asociadas a los nodos
+// ---- Prototipos de Funciones de los nodos ----
+
+/**
+ * @brief Crea un nuevo nodo para la lista de documentos.
+ * Asigna memoria para el nodo.
+ * Inicializa la frecuencia a 1 y setea el puntero "next" a NULL.
+ * @param documento Es el identificador del documento para el nodo.
+ * @return nodePtr Puntero al nodo recien creado, o NULL si ocurre un error o no se logra asignar memoria.
+ */
 nodePtr crear_nodo(const char* documento);
+
+
+/**
+ * @brief 
+ */
 bool insertar_o_sumar_node(nodePtr* head, const char* documento);
 void free_list(nodePtr* head);
 void print_list(nodePtr head);
--- src/inverted_index.c
+++ src/inverted_index.c
@@ -1,1 +1,2 @@
 #include "./includes/inverted_index.h"
+
